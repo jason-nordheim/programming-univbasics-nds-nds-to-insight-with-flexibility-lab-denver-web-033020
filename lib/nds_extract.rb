9616b717286866667c599852d845f07d6da5e4a0
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-11 16:58:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -51,7 +51,7 @@ def movies_with_director_key(name, movies_collection)
   output = []
   k = 0
   while k < movies_collection.length do
-    movie = movies_collection[i][:director_name]
+    dir = movies_collection[i][:director_name]
     ouput << movie_with_director_name()
     k += 1
   end
