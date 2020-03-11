8a063154332bd4bbb834b1729e5b62e6fd927130
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-11 16:51:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -102,6 +102,12 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  k = 0
+  while k < source.count do
+
+    k += 1 
+  end
+
 end
 
 # ----------------    End of Your Code Region --------------------
